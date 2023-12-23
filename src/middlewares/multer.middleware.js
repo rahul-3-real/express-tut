a9ae3012e1d82e500ca18963a25069c765e3331b
Rahul Yadav <rahul.yadav6131766@gmail.com>
2023-12-23 15:03:47 +0530

    Multer - Uploading files wit file extension

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,8 +5,10 @@ const storage = multer.diskStorage({
     cb(null, "./public");
   },
   filename: function (req, file, cb) {
+    let extArray = file.mimetype.split("/");
+    let extension = extArray[extArray.length - 1];
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, file.fieldname + "-" + uniqueSuffix + "." + extension);
   },
 });
 
