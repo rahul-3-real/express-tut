d28230c05db39ae229d310b82c8f89275a6efaad
Rahul Yadav <rahul.yadav6131766@gmail.com>
2023-12-24 19:25:25 +0530

    removed unwanted console.log

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -421,8 +421,6 @@ export const getUserChannelProfile = asyncHandler(async (req, res) => {
     },
   ]);
 
-  console.log(channel);
-
   if (!channel?.length) {
     throw new ApiError(404, "Channel does not exist!");
   }
